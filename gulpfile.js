c9f456557fffcd157c9fd5ee3193ff82012464dd
Donovan Mueller <zotobi@gmail.com>
2017-07-14 16:30:54 -0700

    Clean up gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp')
 const ts = require('gulp-typescript')
-const JSON_FILES = ['src/*.json', 'src/**/*.json']
 
 // pull in the project TypeScript config
 const tsProject = ts.createProject('tsconfig.json')
@@ -15,9 +14,4 @@ gulp.task('watch', ['scripts'], () => {
 	gulp.watch('src/**/*.ts', ['scripts'])
 })
 
-gulp.task('assets', function() {
-	return gulp.src(JSON_FILES)
-		.pipe(gulp.dest('dist'))
-})
-
-gulp.task('default', ['watch', 'assets'])
+gulp.task('default', ['watch'])
